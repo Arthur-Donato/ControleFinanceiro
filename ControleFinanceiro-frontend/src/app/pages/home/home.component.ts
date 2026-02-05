19d3da97c919ba4839a685567be3b7214023dd5b
Arthur Barbosa Donato <142551254+Arthur-Donato@users.noreply.github.com>
2026-02-04 21:45:16 -0300

    feature - Adicionando função no botão de logOut na tela home

--- ControleFinanceiro-frontend/src/app/pages/home/home.component.ts
+++ ControleFinanceiro-frontend/src/app/pages/home/home.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../../services/api.service';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
 
+  constructor(
+
+    private service: ApiService,
+    private router: Router
+  ){}
 }
